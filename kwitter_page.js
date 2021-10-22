1771ec4e4b4894cbf82555d84b10719c138b187b
Ishithareddy123 <81242657+Ishithareddy123@users.noreply.github.com>
2021-10-22 17:49:10 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -13,23 +13,40 @@ const firebaseConfig = {
     firebase.initializeApp(firebaseConfig);
 
 user_name=localStorage.getItem("user name ");
-room_name=localStorage.getItem("roomname");
+room_name=localStorage.getItem("room name");
 console.log(user_name);
 console.log(room_name);
 function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
          firebase_message_id = childKey;
          message_data = childData;
 //Start code
-
+console.log(firebase_message_id);
+console.log(message_data);
+user=message_data['name'];
+message=message_data['message'];
+like=message_data['likes'];
+message_name_tag="<h4>"+user+"<img class='user_tick' src='tick.png'> </h4>";
+message_body="<h4 class='message_h4'>"+message+"</h4>";
+message_likes="<button class='btn btn-warning' value="+like+" id="+firebase_message_id+" onclick='update_likes(this.id)'>";
+button_content="<span class='glyphicon glyphicon-thumbs-up'> Likes: "+like+"</span></button><hr>";
+row=message_name_tag+message_body+message_likes+button_content;
+document.getElementById("output").innerHTML+=row;
 //End code
       } });  }); }
 getData();
-
+function update_likes(message_id){
+      console.log(message_id);
+  likes=document.getElementById(message_id).value;
+  likes=Number(likes)+1;
+  firebase.database().ref(room_name).child(message_id).update({
+        likes:likes
+  });
+}
 function send(){
       msg=document.getElementById("message").value;
       firebase.database().ref(room_name).push({
             name:user_name,
-            meassage:msg,
+            message:msg,
             likes:0
       });
       document.getElementById("message").value="";
